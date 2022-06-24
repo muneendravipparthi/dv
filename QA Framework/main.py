2356914b5ded7a1605bb51a6ffc2123bef3eba23
cb-muneendra <muneendra@chargebee.com>
2022-06-24 19:10:17 +0530

    Ellomi iupdated scriptchanges

--- QA Framework/main.py
+++ QA Framework/main.py
@@ -1,5 +1,7 @@
 import re
 
+import pandas as pd
+
 from SupportingFunctions import *
 from CompareData import *
 from ReportGeneration import *
@@ -45,27 +47,41 @@ def main():
                 source_df = read_data_from_file(source_files, source_columns)
 
             destination_df = read_data_from_file(destination_files, destination_columns)
+            #For Eloomi
+            itemids = ["1_9", "3_3", "1_18", "1_8", "3_72", "1_15", "1_6", "1_10", "1_16", "3_71", "1_19", "1_13", "6_3", "3_74", "3_65", "1_11", "3_66", "8_2", "6_1", "1_17", "5_1", "1_14", "8_1", "6_4", "8_3", "3_61", "1_20", "3_73", "3_67", "6_2", "3_40"]
+            source_df_temp = source_df.copy()
+            destination_df_temp = destination_df.copy()
+            overalldiff = pd.DataFrame()
+            for itemid in itemids:
+            # itemid = '1_13'
+                source_df = source_df[source_df['Item_ID'] == itemid]
+                destination_df = destination_df[destination_df['Item_Id'] == itemid]
 
-            # Filter src and dst
-            source_df, destination_df = filterData(source_df, destination_df, src_key, des_key)
+                # Filter src and dst
+                source_df, destination_df = filterData(source_df, destination_df, src_key, des_key)
 
-            # pre-validation an d formatting of the data is common
-            source_df, destination_df = pre_validation_check(type_in, source_df, source_columns, destination_df, destination_columns)
+                # pre-validation an d formatting of the data is common
+                source_df, destination_df = pre_validation_check(type_in, source_df, source_columns, destination_df, destination_columns)
 
-            source_df = source_df.sort_values(by=src_key)
-            destination_df = destination_df.sort_values(by=des_key)
+                source_df = source_df.sort_values(by=src_key)
+                destination_df = destination_df.sort_values(by=des_key)
 
-            # Missing Data Capture Information
+                # Missing Data Capture Information
 
-            # #Filter src and dst
-            # source_df, destination_df = filterData(source_df, destination_df,src_key,des_key)
-            # compare and report  code is common
-            diff_df = compare_data(source_df, destination_df)  # compare
+                # #Filter src and dst
+                # source_df, destination_df = filterData(source_df, destination_df,src_key,des_key)
+                # compare and report  code is common
+                diff_df = compare_data(source_df, destination_df)  # compare
+                overalldiff = overalldiff.append(diff_df)
+                source_df = source_df_temp.copy()
+                destination_df = destination_df_temp.copy()
+                print("---------------validation_",itemid,"_completed-----------------")
+
             site_name, client_name = get_details('Site', 'ClientName')
             sheet_name = str(type_of_execution) + str(sheetname[counter])
-            report_generation(type_in, diff_df, site_name, client_name, module[counter], sheet_name)  # Report
-
+            report_generation(type_in, overalldiff, site_name, client_name, module[counter], sheet_name)  # Report
             print("--------------------------------")
+
         else:
             print('Reading and validation is SET to FALSE')
             print("--------------------------------")
