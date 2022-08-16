0dd2bb9fcaa310cb4af737d76dd5dbca1f504a7e
cb-muneendra <muneendra@chargebee.com>
2022-08-16 17:48:05 +0530

    Nsaas QA execution properties updated

--- utils/DropEmptyColumns.py
+++ utils/DropEmptyColumns.py
@@ -1,7 +1,7 @@
 # importing libraries
 import pandas as pd
 
-dataFile = '/Users/cb-muneendra/Desktop/leeto_Live_subscription_expected.xlsx'
+dataFile = '/Users/cb-muneendra/Desktop/Nsaas_Invoice_DS2.xlsx'
 
 if dataFile.endswith('csv'):
     df = pd.read_csv(dataFile)
