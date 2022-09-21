16200917059dfbf558beb7bc2976b279cf610c00
cb-muneendra <muneendra@chargebee.com>
2022-09-21 12:32:46 +0530

    Updated during execution

--- utils/DropEmptyColumns.py
+++ utils/DropEmptyColumns.py
@@ -1,7 +1,7 @@
 # importing libraries
 import pandas as pd
 
-dataFile = '/Users/cb-muneendra/Desktop/leeto_Live_subscription_expected.xlsx'
+dataFile = '/Users/cb-muneendra/Downloads/Microshare_QA - Invoice-UK.csv'
 
 if dataFile.endswith('csv'):
     df = pd.read_csv(dataFile)
