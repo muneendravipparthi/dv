b022350880adf7433bd0134a53eac9c3bce121c6
cb-muneendra <muneendra@chargebee.com>
2022-09-26 17:08:13 +0530

    Updated

--- utils/DropEmptyColumns.py
+++ utils/DropEmptyColumns.py
@@ -1,7 +1,7 @@
 # importing libraries
 import pandas as pd
 
-dataFile = '/Users/cb-muneendra/Desktop/leeto_Live_subscription_expected.xlsx'
+dataFile = '/Users/cb-muneendra/Downloads/Tubules_Chargebee Migration Template (PC 2.0) - Subscriptions for customers.csv'
 
 if dataFile.endswith('csv'):
     df = pd.read_csv(dataFile)
